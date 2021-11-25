df9c4752acffba8baedc21bf3d9c13a8e776ce62
Jack Lin <knight131991@yahoo.com.tw>
2021-11-25 20:50:11 +0800

    fetch estimated time per 10 second

--- src/busStopInfo/BusStopInfo.jsx
+++ src/busStopInfo/BusStopInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 // import PropTypes from "prop-types";
 import queryString from "query-string";
 import useApiAdapter from "../hooks/useApiAdapter";
@@ -8,6 +8,7 @@ import { Spin, Steps, Tabs } from "antd";
 import calEstimatedTime from "./calEstimatedTime";
 
 function BusStopInfo(props) {
+  const [count, setCount] = useState(0);
   const {
     apiAdapter: getRouteInfo,
     isLoading,
@@ -22,6 +23,17 @@ function BusStopInfo(props) {
   const { city, route } = queryString.parse(history.location.search);
   const { TabPane } = Tabs;
   const { Step } = Steps;
+
+  const handleGetEstimatedTime = useCallback(() => {
+    getEstimatedTime({
+      api: axios.get(
+        `https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/${city}/${encodeURIComponent(
+          route
+        )}`
+      ),
+      mapper: (resp) => calEstimatedTime(resp.data),
+    });
+  }, [getEstimatedTime, city, route]);
 
   useEffect(() => {
     getRouteInfo({
@@ -38,15 +50,20 @@ function BusStopInfo(props) {
   }, [getRouteInfo, city, route]);
 
   useEffect(() => {
-    getEstimatedTime({
-      api: axios.get(
-        `https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/${city}/${encodeURIComponent(
-          route
-        )}`
-      ),
-      mapper: (resp) => calEstimatedTime(resp.data),
-    });
-  }, [getEstimatedTime, city, route]);
+    handleGetEstimatedTime();
+    const interval = setInterval(() => {
+      setCount(0);
+      handleGetEstimatedTime();
+    }, 10000);
+    return () => clearInterval(interval);
+  }, [handleGetEstimatedTime]);
+
+  useEffect(() => {
+    const counter = setInterval(() => {
+      setCount(count + 1);
+    }, 1000);
+    return () => clearInterval(counter);
+  }, [count]);
 
   return (
     <Spin spinning={isLoading}>
@@ -54,7 +71,11 @@ function BusStopInfo(props) {
         {/* TODO 預期routerInfo只有2個item 但有時後端提供的資料會超過2個 所以先取前兩個 */}
         {routeInfo.slice(0, 2).map((stops, id) => {
           return (
-            <TabPane key={id} tab={`往 ${stops.at(-1).StopName.Zh_tw}`}>
+            <TabPane
+              key={id}
+              tab={`往 ${stops[stops.length - 1].StopName.Zh_tw}`}
+            >
+              {`*於 ${count} 秒前更新`}
               <Steps direction="vertical">
                 {stops.map((stop, subId) => (
                   <Step
