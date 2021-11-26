e569d5c0f70e5ed838be5ecbab81d85e4b7f83b3
Jack Lin <knight131991@yahoo.com.tw>
2021-11-27 01:22:19 +0800

    implement SearchBusPage for mobile

--- src/searchBusPage/SearchBusPage.jsx
+++ src/searchBusPage/SearchBusPage.jsx
@@ -10,10 +10,11 @@ import CommonList from "../components/commonList/CommonList";
 import LogoBtn from "../components/LogoBtn";
 import styled from "styled-components";
 import { ReactComponent as Del } from "../images/del.svg";
+import useKeyboardItems from "./useKeyboardItems";
 // import PropTypes from 'prop-types'
 
 const Container = styled(FlexBox)`
-  padding: 25px 100px;
+  ${({ bpoint }) => bpoint !== "xs" && "padding: 25px 100px"};
   background-color: #131414;
   height: 100%;
   color: white;
@@ -22,6 +23,7 @@ const Container = styled(FlexBox)`
 const LogoBar = styled(FlexBox)`
   gap: 10px;
   margin-bottom: 15px;
+  ${({ bpoint }) => bpoint === "xs" && "padding: 18px;"}
 
   & > input {
     height: 40px;
@@ -36,8 +38,10 @@ const KeyboardContainer = styled(GridRow)`
   padding: 24px 16px;
   background-color: #1c1d1d;
   border-radius: 10px;
-  min-height: 550px;
+  min-height: ${({ bpoint }) => bpoint !== "xs" && "550px"}
   align-content: flex-start;
+
+  ${({ bpoint }) => bpoint === "xs" && "position: absolute; bottom: 0px;"}
 
   & > div {
     padding: 6px;
@@ -58,42 +62,20 @@ const LeftContainer = styled(FlexBox)`
   margin-right: 42px;
 `;
 
-function SearchBusPage(props) {
+function SearchBusPage({ bpoint }) {
   const history = useHistory();
   const inputEle = useRef();
   const [search, setSearch] = useState("");
   const [city, setCity] = useState(cityList[0]);
   const [showCitySelector, setShowCitySelector] = useState(false);
 
-  const appendToSearch = useCallback(
-    (val) => setSearch(search.concat(val)),
-    [search, setSearch]
+  const keyboardItems = useKeyboardItems(
+    search,
+    setSearch,
+    setShowCitySelector,
+    city,
+    inputEle
   );
-
-  const keyboardItems = [
-    { event: () => setShowCitySelector(true), span: 60, label: city.label },
-    { event: () => inputEle.current.focus(), span: 40, label: "手動輸入" },
-    { event: () => appendToSearch("紅"), span: 20, label: "紅" },
-    { event: () => appendToSearch("藍"), span: 20, label: "籃" },
-    { event: () => appendToSearch("1"), span: 20, label: "1" },
-    { event: () => appendToSearch("2"), span: 20, label: "2" },
-    { event: () => appendToSearch("3"), span: 20, label: "3" },
-    { event: () => appendToSearch("綠"), span: 20, label: "綠" },
-    { event: () => appendToSearch("棕"), span: 20, label: "棕" },
-    { event: () => appendToSearch("4"), span: 20, label: "4" },
-    { event: () => appendToSearch("5"), span: 20, label: "5" },
-    { event: () => appendToSearch("6"), span: 20, label: "6" },
-    { event: () => appendToSearch("橘"), span: 20, label: "橘" },
-    { event: () => appendToSearch("小"), span: 20, label: "小" },
-    { event: () => appendToSearch("7"), span: 20, label: "7" },
-    { event: () => appendToSearch("8"), span: 20, label: "8" },
-    { event: () => appendToSearch("9"), span: 20, label: "9" },
-    { event: () => appendToSearch("幹線"), span: 20, label: "幹線" },
-    { event: () => appendToSearch("more"), span: 20, label: "更多" },
-    { event: () => appendToSearch(0), span: 20, label: "C" },
-    { event: () => appendToSearch("0"), span: 20, label: "0" },
-    { event: () => setSearch(search.slice(0, -1)), span: 20, label: <Del /> },
-  ];
 
   const { apiAdapter, isLoading, data } = useApiAdapter([]);
 
@@ -123,44 +105,70 @@ function SearchBusPage(props) {
     });
   }, [search, city.value, apiAdapter, history]);
 
+  const LogoBarComponent = ({ bpoint }) => (
+    <LogoBar row align="end" bpoint={bpoint}>
+      <LogoBtn bpoint={bpoint} />
+      <Input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        ref={inputEle}
+      />
+    </LogoBar>
+  );
+
+  const KeyboardComponent = ({ bpoint }) => (
+    <KeyboardContainer row bpoint={bpoint}>
+      {showCitySelector
+        ? cityList.map(({ value, label }) => (
+            <GridCol key={value} span={20}>
+              <Button
+                onClick={() => {
+                  setCity({ value, label });
+                  setShowCitySelector(false);
+                }}
+              >
+                {label}
+              </Button>
+            </GridCol>
+          ))
+        : keyboardItems.map(({ span, label, event }, id) => (
+            <GridCol key={id} span={span}>
+              <Button onClick={event}>{label}</Button>
+            </GridCol>
+          ))}
+    </KeyboardContainer>
+  );
+
+  const List = styled((props) => (
+    <div {...props}>
+      <Spin spinning={isLoading}>
+        {city.label}
+        <CommonList dataSource={data} />
+      </Spin>
+    </div>
+  ))`
+    padding: 8px 18px;
+  `;
+
   return (
-    <Container row>
-      <LeftContainer>
-        <LogoBar row align="end">
-          <LogoBtn />
-          <Input
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            ref={inputEle}
-          />
-        </LogoBar>
-        <KeyboardContainer row>
-          {showCitySelector
-            ? cityList.map(({ value, label }) => (
-                <GridCol key={value} span={20}>
-                  <Button
-                    onClick={() => {
-                      setCity({ value, label });
-                      setShowCitySelector(false);
-                    }}
-                  >
-                    {label}
-                  </Button>
-                </GridCol>
-              ))
-            : keyboardItems.map(({ span, label, event }, id) => (
-                <GridCol key={id} span={span}>
-                  <Button onClick={event}>{label}</Button>
-                </GridCol>
-              ))}
-        </KeyboardContainer>
-      </LeftContainer>
-      <FlexBox flex>
-        <Spin spinning={isLoading}>
-          {city.label}
-          <CommonList dataSource={data} />
-        </Spin>
-      </FlexBox>
+    <Container row={bpoint !== "xs"} bpoint={bpoint}>
+      {bpoint === "xs" ? (
+        <>
+          <LogoBarComponent bpoint={bpoint} />
+          <List bpoint={bpoint} />
+          <KeyboardComponent bpoint={bpoint} />
+        </>
+      ) : (
+        <>
+          <LeftContainer>
+            <LogoBarComponent />
+            <KeyboardComponent />
+          </LeftContainer>
+          <FlexBox flex>
+            <List />
+          </FlexBox>
+        </>
+      )}
     </Container>
   );
 }
---
+++ src/searchBusPage/useKeyboardItems.jsx
@@ -0,0 +1,40 @@
+import { useCallback } from "react";
+import { ReactComponent as Del } from "../images/del.svg";
+
+export default function useKeyboardItems(
+  search,
+  setSearch,
+  setShowCitySelector,
+  city,
+  inputEle
+) {
+  const appendToSearch = useCallback(
+    (val) => setSearch(search.concat(val)),
+    [search, setSearch]
+  );
+
+  return [
+    { event: () => setShowCitySelector(true), span: 60, label: city.label },
+    { event: () => inputEle.current.focus(), span: 40, label: "手動輸入" },
+    { event: () => appendToSearch("紅"), span: 20, label: "紅" },
+    { event: () => appendToSearch("藍"), span: 20, label: "籃" },
+    { event: () => appendToSearch("1"), span: 20, label: "1" },
+    { event: () => appendToSearch("2"), span: 20, label: "2" },
+    { event: () => appendToSearch("3"), span: 20, label: "3" },
+    { event: () => appendToSearch("綠"), span: 20, label: "綠" },
+    { event: () => appendToSearch("棕"), span: 20, label: "棕" },
+    { event: () => appendToSearch("4"), span: 20, label: "4" },
+    { event: () => appendToSearch("5"), span: 20, label: "5" },
+    { event: () => appendToSearch("6"), span: 20, label: "6" },
+    { event: () => appendToSearch("橘"), span: 20, label: "橘" },
+    { event: () => appendToSearch("小"), span: 20, label: "小" },
+    { event: () => appendToSearch("7"), span: 20, label: "7" },
+    { event: () => appendToSearch("8"), span: 20, label: "8" },
+    { event: () => appendToSearch("9"), span: 20, label: "9" },
+    { event: () => appendToSearch("幹線"), span: 20, label: "幹線" },
+    { event: () => appendToSearch("more"), span: 20, label: "更多" },
+    { event: () => appendToSearch(0), span: 20, label: "C" },
+    { event: () => appendToSearch("0"), span: 20, label: "0" },
+    { event: () => setSearch(search.slice(0, -1)), span: 20, label: <Del /> },
+  ];
+}
